56893069585fcaeb74bea48de094770f12928d49
Mike Heitmann, N0SO <n0so@arrl.net>
2022-07-25 23:08:43 -0500

    Adding test files

--- qso.py
+++ qso.py
@@ -205,32 +205,3 @@ class dbQSO(QSO):
         self.noqsos = qso['NOQSOS']
         return True
                     
-
-if __name__ == '__main__':
-    """Test code goes here"""
-    tt=dbQSO()
-    print('dict:\n{}'.format(dir(tt)))
-    print('vars:\n{}'.format(vars(tt)))
-    print('keys:\n{}'.format(vars(tt).keys()))
-    
-    tt = dbQSO(qdata = '7074 FT8 2022-07-25 1715 N0SO -10 EM48 AB0RX -05 EM39')
-    tt = dbQSO(qdata = {'FREQ':7074,
-                        'MODE':'FT8',
-                        'DATETIME': (2022, 7, 25, 17, 15),
-                        'MYCALL':'N0SO',
-                        'MYREPORT': -10,
-                        'MYQTH':'EM48',
-                        'URCALL':'AB0RX',
-                        'URREPORT': -5,
-                        'URQTH':'EM39',
-                        'ID': 1,
-                        'LOGID': 100,
-                        'QSL': 300,
-                        'VALID': 1,
-                        'NOQSOS': 0,
-                        'NOLOG': 1,
-                        'DUPE': 0
-                        })
-#    tt = dbQSO(freq=7074)
-    print ('populated tt:\n{}'.format(vars(tt)))
-   
---
+++ test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Unit test code"""
+import sys
+sys.path.insert(0, '/home/pi/Projects/moqputils')
+#print (sys.path)
+from qso import *
+if __name__ == '__main__':
+    """Test code goes here"""
+    import sys
+    print(sys.path)
+    tt=dbQSO()
+    print('dict:\n{}'.format(dir(tt)))
+    print('vars:\n{}'.format(vars(tt)))
+    print('keys:\n{}'.format(vars(tt).keys()))
+    
+    tt = dbQSO(qdata = '7074 FT8 2022-07-25 1715 N0SO -10 EM48 AB0RX -05 EM39')
+    tt = dbQSO(qdata = {'FREQ':7074,
+                        'MODE':'FT8',
+                        'DATETIME': (2022, 7, 25, 17, 15),
+                        'MYCALL':'N0SO',
+                        'MYREPORT': -10,
+                        'MYQTH':'EM48',
+                        'URCALL':'AB0RX',
+                        'URREPORT': -5,
+                        'URQTH':'EM39',
+                        'ID': 1,
+                        'LOGID': 100,
+                        'QSL': 300,
+                        'VALID': 1,
+                        'NOQSOS': 0,
+                        'NOLOG': 1,
+                        'DUPE': 0
+                        })
+#    tt = dbQSO(freq=7074)
+    print ('populated tt:\n{}'.format(vars(tt)))
+   
